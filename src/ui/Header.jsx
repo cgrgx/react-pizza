c93b577c3895107e46794d1b2528f8210f1eccd7
chiran gurung <grgchiran08@yahoo.com>
2023-12-13 23:33:03 -0500

    building the App Layout

---
+++ src/ui/Header.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">React Pizza</Link>
+
+      <p>as</p>
+    </header>
+  );
+}
+
+export default Header;
